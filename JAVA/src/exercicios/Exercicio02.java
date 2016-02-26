3b5aaaa0d09acb8f5a30ac523f5d0ea76b363895
Alessandro <Alessandro@Alessandro>
2016-02-26 14:42:14 -0300

    Exercicíos Corrigidos 26/02/2016

--- JAVA/src/exercicios/Exercicio02.java
+++ JAVA/src/exercicios/Exercicio02.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercicios;
 
 import java.util.Scanner;
 
 /**
+ * 2) Crie uma classe para calcular a área de um triângulo.
  *
  * @author Alessandro
  */
