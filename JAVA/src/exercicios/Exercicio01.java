dd62bb9873ccb0d90f487bf97d9e20521e32a765
Alessandro <Alessandro@Alessandro>
2016-02-26 13:15:20 -0300

    Comit Inicial

---
+++ JAVA/src/exercicios/Exercicio01.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+/**
+ * Crie uma classe para calcular a área de um retângulo.
+ *
+ * @author Alessandro
+ */
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        double base, altura, area;
+        Scanner leia = new Scanner(System.in);
+        System.out.print("Digite a base: ");
+        base = leia.nextDouble();
+        System.out.print("Digite a altura: ");
+        altura = leia.nextDouble();
+        area = base * altura;
+        System.out.println("A Área é: " + area);
+    }
+}
