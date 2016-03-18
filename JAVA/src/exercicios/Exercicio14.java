b4d5172025f6da9b3ac35ee5975e8314b6730720
Alessandro <Alessandro@Alessandro>
2016-03-18 13:04:33 -0300

    Aula do dia 18/03/2016

---
+++ JAVA/src/exercicios/Exercicio14.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+/** 
+*
+* @author Alessandro
+* @create: 18/03/2016, 12:45:26
+*/
+public class Exercicio14 {
+    public static void main(String[] args) {
+        for (char letra = 'A'; letra <= 'Z'; letra++) {
+            System.out.print(letra+ " ");
+        }
+    }
+}
