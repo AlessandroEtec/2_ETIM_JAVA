454037f2438d864558cd929f3e25defae63ab6fa
Alessandro <Alessandro@Alessandro>
2016-03-18 13:06:44 -0300

    Aula do dia 08/04/2016

---
+++ JAVA/src/vetor/Exemplo1.java
@@ -0,0 +1,29 @@
+package vetor;
+
+/**
+ *
+ * @author Alessandro
+ * @create: 08/04/2016, 12:49:26
+ */
+public class Exemplo1 {
+
+    public static void main(String[] args) {
+        //declaração do vetor
+        int[] numeros = new int[5];
+
+        //atribuição de valores
+        numeros[0] = 3;
+        numeros[1] = 2;
+        numeros[2] = 5;
+        numeros[3] = 7;
+        numeros[4] = 1;
+
+        //Exibe valores do vetor
+        System.out.println("Posição 0: " + numeros[0]);
+        System.out.println("Posição 1: " + numeros[1]);
+        System.out.println("Posição 2: " + numeros[2]);
+        System.out.println("Posição 3: " + numeros[3]);
+        System.out.println("Posição 4: " + numeros[4]);
+
+    }
+}
