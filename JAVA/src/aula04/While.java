16af89a13a3a8425e9e32ca073f8f53f32660d54
Alessandro <Alessandro@Alessandro>
2016-03-18 13:05:39 -0300

    Aula do dia 04/03/2016

---
+++ JAVA/src/aula04/While.java
@@ -0,0 +1,18 @@
+package aula04;
+
+
+/**
+ *
+ * @author Alessandro
+ * @create: 04/03/2016, 13:46:05
+ */
+public class While {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i + " ");
+            i = i + 1;
+        }
+    }
+}
