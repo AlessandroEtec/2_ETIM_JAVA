7b2c438095184123c9e46b034f667f70f3bf50a2
Alessandro <Alessandro@Alessandro>
2016-04-29 16:40:14 -0300

    Aula do dia 08/04/2016

---
+++ JAVA/src/vetor/Exemplo2.java
@@ -0,0 +1,24 @@
+package vetor;
+
+/**
+ *
+ * @author Alessandro
+ * @create: 08/04/2016, 12:50:14
+ */
+public class Exemplo2 {
+
+    public static void main(String[] args) {
+        int numeros[] = {0, 2, 3, 4, 1};
+        System.out.println("tamanho do vetor: " + numeros.length);
+
+        //Exibindo com for
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+        
+        //Exibindo com foreach
+        for (int n : numeros) {
+            System.out.println(n);
+        }
+    }
+}
