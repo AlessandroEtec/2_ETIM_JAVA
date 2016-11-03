669053d1d3d1c8c357756766755a4ede0bf4326a
Alessandro <Alessandro@Alessandro>
2016-11-03 13:09:24 -0200

    Alteração de Registros

---
+++ AGENDA/alterar.php
@@ -0,0 +1,28 @@
+
+<?php
+$conexao = new mysqli("localhost", "root", "etec");
+$conexao->set_charset("UTF8");
+if ($conexao->connect_error) {
+    die("Falha ao conectar: " . $conexao->connect_error);
+}
+if (!$conexao->select_db("agenda")) {
+    die("O Banco de dados não existe");
+}
+$codigo = $_GET["codigo"];
+$nome = $_GET["nome"];
+$endereco = $_GET["endereco"];
+$cidade = $_GET["cidade"];
+$fone = $_GET["fone"];
+
+$sql = "update pessoa set nome = '$nome' , endereco = '$endereco'," .
+        "cidade = '$cidade' , telefone =  '$fone' where codigo = $codigo";
+$status = $conexao->query($sql);
+if ($status == 0) {
+    echo "Erro ao inserir: " . $conexao->error;
+} else if ($status == 1) {
+    echo "<script>alert('Registro Alterado com Sucesso')</script>"
+    . "<meta HTTP-EQUIV='Refresh' CONTENT='0;URL=index.php'>";
+}
+$conexao->close();
+?>
+
---
+++ AGENDA/formularioAlterar.php
@@ -0,0 +1,30 @@
+<?php
+$codigo = $_GET["codigo"];
+echo "Código: " . $codigo . "<br>";
+
+$conexao = new mysqli("localhost", "root", "etec");
+$conexao->set_charset("UTF8");
+if ($conexao->connect_error) {
+    die("Falha ao conectar: " . $conexao->connect_error);
+}
+if (!$conexao->select_db("agenda")) {
+    die("O Banco de dados não existe");
+}
+$sql = "SELECT * FROM pessoa where codigo = $codigo";
+$resultado = $conexao->query($sql);
+if ($resultado) {
+    $linha = $resultado->fetch_array();
+    $nome = $linha["nome"];
+    $endereco = $linha["endereco"];
+    $cidade = $linha["cidade"];
+    $telefone =  $linha["telefone"];
+}
+?>
+<form method="GET" action="alterar.php">
+    Nome: <input type="text" name="nome" value="<?=$nome?>"><br>
+    Endereço: <input type="text" name="endereco" value="<?=$endereco?>"><br>
+    Cidade: <input type="text" name="cidade" value="<?=$cidade?>"><br>
+    Telefone: <input type="text" name="fone" value="<?=$telefone?>"><br>
+    <input type="submit" value="Alterar">
+</form>
+
