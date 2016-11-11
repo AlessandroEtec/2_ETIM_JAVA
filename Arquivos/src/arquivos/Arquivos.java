d8591ade9068f952d14abc586b402d8cce755038
Alessandro <Alessandro@Alessandro>
2016-11-11 13:32:07 -0200

    Leitura e Escrita de Arquivos

--- Arquivos/src/arquivos/Arquivos.java
+++ Arquivos/src/arquivos/Arquivos.java
@@ -6,8 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
